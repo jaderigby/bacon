c279259857f2b5933a1c24ba362b4d09516ab4e9
Jade Rigby <jrigby@squareup.com>
2020-12-09 12:03:01 -0700

    changing intro message on messages.py file

--- template/messages.py
+++ template/messages.py
@@ -2,6 +2,11 @@ def example():
 	print "process working!"
 
 def statusMessage():
-	print
-	print "<tool-name> is working successfully!"
-	print
+	print '''
+Test is working successfully!
+
+You may edit this message by going to your utility's 
+"message.py" file and modifying the "statusMessage" function.
+Add a welcome message or a help menu, such as adding 
+a list of your utilities possible commands.
+'''
