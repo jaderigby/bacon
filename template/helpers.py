7aeba4390afe486bc49dbf4b3d537f74d5393929
Jade Rigby <contact@jaderigby.com>
2021-01-31 19:28:56 -0700

    cleaned up whitespace on kv_set

--- template/helpers.py
+++ template/helpers.py
@@ -137,7 +137,7 @@ def user_selection(DESCRIPTION, LIST, LIST_SELECT = False):
 def arguments(ARGS, DIVIDER=':'):
 	return dict(item.split('{}'.format(DIVIDER)) for item in ARGS)
 
-def kv_set(DICT, KEY, DEFAULT=False):
+def kv_set(DICT, KEY, DEFAULT = False):
 	if KEY in DICT:
 		if DICT[KEY] == 'true':
 			DICT[KEY] = 't'
